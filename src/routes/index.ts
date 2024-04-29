269bf039060cb3b9d814b739fa688d7784e51235
Deemanth Poonacha <deemanth.poonacha@ikara.ai>
2024-04-29 13:27:02 +0530

    Root route updated

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -3,6 +3,7 @@ import authRouter from "./auth.routes";
 import userRouter from "./user.routes";
 const router = express.Router();
 
+router.get("/", (req, res) => res.send("Express on Vercel"));
 router.get("/ping", (req, res) => res.sendStatus(200));
 router.use(userRouter);
 router.use(authRouter);
